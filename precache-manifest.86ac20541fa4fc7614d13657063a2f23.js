0f9afe54235b0351aa1e9dc7282533bc4eee0388
Eric Schubert <49171315+bernardericschubert@users.noreply.github.com>
2019-08-25 22:37:49 -0700

    Updates

---
+++ precache-manifest.86ac20541fa4fc7614d13657063a2f23.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c378372bca9ea1a89d4c5b8af0dcd475",
+    "url": "/clickygame/index.html"
+  },
+  {
+    "revision": "648efb2905e01db11d7c",
+    "url": "/clickygame/static/css/main.14c7c997.chunk.css"
+  },
+  {
+    "revision": "c781d8d14aa25daf4a98",
+    "url": "/clickygame/static/js/2.0fb7f9dc.chunk.js"
+  },
+  {
+    "revision": "648efb2905e01db11d7c",
+    "url": "/clickygame/static/js/main.b00be6a2.chunk.js"
+  },
+  {
+    "revision": "10ad61c5a1323c584565",
+    "url": "/clickygame/static/js/runtime~main.74bdfeca.js"
+  }
+]);
